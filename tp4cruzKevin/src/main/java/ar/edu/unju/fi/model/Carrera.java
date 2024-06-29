1ed9ac53c7a72acf1fd8b9000860228c186511b1
Joel Cruz <Joel Cruz@DESKTOP-ITJELBR>
2024-06-28 21:37:10 -0300

    Finalizacion de los models

--- tp4cruzKevin/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4cruzKevin/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,5 +1,55 @@
 package ar.edu.unju.fi.model;
 
 public class Carrera {
+	private String codigo;
+	private String nombre;
+	private int cantAños;
+	private boolean estado;
+	
+	public Carrera() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Carrera(String codigo, String nombre, int cantAños, boolean estado) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantAños = cantAños;
+		this.estado = estado;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantAños() {
+		return cantAños;
+	}
+
+	public void setCantAños(int cantAños) {
+		this.cantAños = cantAños;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	
 }
+
--- tp4cruzKevin/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4cruzKevin/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,5 +1,67 @@
 package ar.edu.unju.fi.model;
-
 public class Materia {
-
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private int horas;
+	private String modalidad;
+	private Docente docente;
+	private Carrera carrera;
+	public Materia() {
+		// TODO Auto-generated constructor stub
+	}
+	public Materia(String codigo, String nombre, String curso, int horas, String modalidad, Docente docente,
+			Carrera carrera) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.curso = curso;
+		this.horas = horas;
+		this.modalidad = modalidad;
+		this.docente = docente;
+		this.carrera = carrera;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public int getHoras() {
+		return horas;
+	}
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	public String getModalidad() {
+		return modalidad;
+	}
+	public void setModalidad(String modalidad) {
+		this.modalidad = modalidad;
+	}
+	public Docente getDocente() {
+		return docente;
+	}
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+	public Carrera getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+	
 }
